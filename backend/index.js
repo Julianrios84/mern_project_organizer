4780a2b4d70e984ce2e9d1719f6698a6532980ec
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-24 23:20:59 -0500

    Configurando variables de entorno y gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import database from './config/db.js'
+
 const app = express()
+const port = process.env.PORT || 4000
 
+dotenv.config()
 
-app.listen(4000, () => {
-  console.log('Server running...')
+database();
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`)
 })
+
+
