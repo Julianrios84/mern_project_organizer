9e2cf4b5e46ddcc29b5d883cc3d0d81f543f7ea9
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-24 23:57:23 -0500

    Creando el routing de usuarios

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import database from './config/db.js'
+import userRoutes from './routes/user.route.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -8,6 +9,9 @@ const port = process.env.PORT || 4000
 dotenv.config()
 
 database();
+
+// Routing
+app.use('/api/users', userRoutes)
 
 app.listen(port, () => {
   console.log(`Server running in port ${port}`)
