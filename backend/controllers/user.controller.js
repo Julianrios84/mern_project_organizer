fa84846ab64533f21b83017a3ef2e7fe60f5e02d
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-25 00:31:48 -0500

    Validaciones para la autenticación

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,27 +1,36 @@
-import User from '../models/user.model.js'
-import generateId from '../helpers/generateId.helper.js'
+import User from '../models/user.model.js';
+import generateId from '../helpers/generateId.helper.js';
 
 const register = async (req, res) => {
-
   try {
-
-    const { email } = req.body
-    const exist = User.findOne({ email })
-
-    if(exist) {
-      const error = new Error('Usuario ya registrado')
-      return res.status(400).json({ message: error.message })
+    const { email } = req.body;
+    const exist = User.findOne({ email });
+    if (exist) {
+      const error = new Error('Usuario ya registrado');
+      return res.status(400).json({ message: error.message });
     }
-
-    const user = new User(req.body)
-    user.token = generateId()
-    const result = await user.save()
-    res.json(result)
+    const user = new User(req.body);
+    user.token = generateId();
+    const result = await user.save();
+    res.json(result);
   } catch (error) {
-    console.log("🚀 ~ file: user.controller.js:8 ~ register ~ error", error)
+    console.log('🚀 ~ file: user.controller.js:8 ~ register ~ error', error);
   }
-}
+};
 
-export {
-  register
-}
+const autenticate = async (req, res) => {
+  const { email, password } = req.body
+
+  const user = User.findOne({ email })
+  if(!user) {
+    const error = new Error('El usuario no existe')
+    return res.status(404).json({ message: error.message })
+  }
+
+  if(!user.confirm) {
+    const error = new Error('Tu cuenta no a sido confirmada')
+    return res.status(403).json({ message: error.message })
+  }
+};
+
+export { register, autenticate };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router()
 
-import { register } from '../controllers/user.controller.js'
+import { register, autenticate } from '../controllers/user.controller.js'
 
 router.post('/', register)
+router.post('/login', autenticate)
 
 
 export default router
