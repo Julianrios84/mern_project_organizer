097dcba9eeac9c23244e6f5ace491995cdac786b
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-25 00:14:42 -0500

    Previniendo usuarios  duplicados

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,6 +3,15 @@ import User from '../models/user.model.js'
 const register = async (req, res) => {
 
   try {
+
+    const { email } = req.body
+    const exist = User.findOne({ email })
+
+    if(exist) {
+      const error = new Error('Usuario ya registrado')
+      return res.status(400).json({ message: error.message })
+    }
+
     const user = new User(req.body)
     const result = await user.save()
     res.json(result)
