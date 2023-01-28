727c6cc2f479232bf593cd8925ed70b3166609fc
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-28 18:17:48 -0500

    Creando routing con React Router DOM 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AuthLayout from './layouts/auth.layout'
+import Login from './pages/auth/login.page'
+import Register from './pages/auth/register.page'
+import Confirm from './pages/auth/confirm.page'
+import ForgetPassword from './pages/auth/password/forget.page'
+import ResetPassword from './pages/auth/password/reset.page'
+
 function App() {
   return (
-    <div className="App">
-      <p className="2xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout/>} >
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="reset" element={<ForgetPassword />} />
+          <Route path="reset/:token" element={<ResetPassword />} />
+          <Route path="confirm/:token" element={<Confirm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
