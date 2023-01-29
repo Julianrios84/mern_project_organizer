810ec9af2cff9a62459e8af54352e875502edf78
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-29 13:11:53 -0500

    Creando el State para la Autenticación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthLayout from './layouts/auth.layout'
-import Login from './pages/auth/login.page'
-import Register from './pages/auth/register.page'
-import Confirm from './pages/auth/confirm.page'
-import ForgetPassword from './pages/auth/password/forget.page'
-import ResetPassword from './pages/auth/password/reset.page'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthLayout from './layouts/auth.layout';
+import Login from './pages/auth/login.page';
+import Register from './pages/auth/register.page';
+import Confirm from './pages/auth/confirm.page';
+import ForgetPassword from './pages/auth/password/forget.page';
+import ResetPassword from './pages/auth/password/reset.page';
+import { AuthProvider } from './context/auth.context';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout/>} >
-          <Route index element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="reset" element={<ForgetPassword />} />
-          <Route path="reset/:token" element={<ResetPassword />} />
-          <Route path="confirm/:token" element={<Confirm />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="reset" element={<ForgetPassword />} />
+            <Route path="reset/:token" element={<ResetPassword />} />
+            <Route path="confirm/:token" element={<Confirm />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/auth/login.page.jsx
+++ frontend/src/pages/auth/login.page.jsx
@@ -1,13 +1,61 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import clientAxios from '../../config/axios.config';
+import Alert from '../../components/alert.component';
+import useAuth from '../../hooks/auth.hook';
 
 const Login = () => {
+  const { setAuth } = useAuth();
+
+  const [alert, setAlert] = useState({});
+  const [user, setUser] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ([user.email, user.password].includes('')) {
+      setAlert({
+        message: 'Todos los campose son obligatorios.',
+        error: true
+      });
+      return;
+    }
+    await loginUser();
+  };
+
+  const loginUser = async () => {
+    try {
+      const { data } = await clientAxios.post(`/user/login`, {
+        email: user.email,
+        password: user.password
+      });
+
+      localStorage.setItem('token', data.token);
+      setAlert({});
+      setAuth(data);
+      setUser({ email: '', password: '' });
+    } catch (error) {
+      setAlert({
+        message: error.response.data.message,
+        error: true
+      });
+    }
+  };
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl font-sansita">
         Inicia sesión y administra tus {''}
         <span className="text-slate-700 font-sansita">projectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {alert && <Alert alert={alert} />}
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             className="capitalize text-gray-600 block text-xl font-bold"
@@ -18,8 +66,11 @@ const Login = () => {
           <input
             id="email"
             type="email"
+            name="email"
             placeholder="Email de registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.email}
+            onChange={(e) => handleChange(e)}
           />
         </div>
         <div className="my-5">
@@ -32,8 +83,11 @@ const Login = () => {
           <input
             id="password"
             type="password"
+            name="password"
             placeholder="Contaseña de registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.password}
+            onChange={(e) => handleChange(e)}
           />
         </div>
 
