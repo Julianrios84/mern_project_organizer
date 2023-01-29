57157a2557d7298e88c1e7f4ecf68ad4a53635ef
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-28 21:39:07 -0500

    Añadiendo state al formulario registrar

--- frontend/src/pages/auth/register.page.jsx
+++ frontend/src/pages/auth/register.page.jsx
@@ -1,88 +1,108 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirm: ''
+  });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]:e.target.value });
+  };
+
   return (
     <>
-    <h1 className="text-sky-600 font-black text-6xl font-sansita">
-      Crea tu cuenta y administra tus {''}
-      <span className="text-slate-700 font-sansita">projectos</span>
-    </h1>
-    <form className="my-10 bg-white shadow rounded-lg p-10">
-    <div className="my-5">
-        <label
-          className="capitalize text-gray-600 block text-xl font-bold"
-          htmlFor="name"
-        >
-          Nombre
-        </label>
-        <input
-          id="name"
-          type="text"
-          placeholder="Nombre de registro"
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-        />
-      </div>
-      <div className="my-5">
-        <label
-          className="capitalize text-gray-600 block text-xl font-bold"
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <input
-          id="email"
-          type="email"
-          placeholder="Email de registro"
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-        />
-      </div>
-      <div className="my-5">
-        <label
-          className="capitalize text-gray-600 block text-xl font-bold"
-          htmlFor="password"
-        >
-          Contraseña
-        </label>
-        <input
-          id="password"
-          type="password"
-          placeholder="Contaseña"
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-        />
-      </div>
-      <div className="my-5">
-        <label
-          className="capitalize text-gray-600 block text-xl font-bold"
-          htmlFor="password2"
-        >
-          Confirmar Contraseña
-        </label>
-        <input
-          id="password2"
-          type="password"
-          placeholder="Confirma tu contraseña"
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-        />
-      </div>
+      <h1 className="text-sky-600 font-black text-6xl font-sansita">
+        Crea tu cuenta y administra tus {''}
+        <span className="text-slate-700 font-sansita">projectos</span>
+      </h1>
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="capitalize text-gray-600 block text-xl font-bold"
+            htmlFor="name"
+          >
+            Nombre
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Nombre de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.name}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="capitalize text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.email}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="capitalize text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Contraseña
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Contaseña"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.password}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="capitalize text-gray-600 block text-xl font-bold"
+            htmlFor="confirm"
+          >
+            Confirmar Contraseña
+          </label>
+          <input
+            id="confirm"
+            type="password"
+            name="confirm"
+            placeholder="Confirma tu contraseña"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={user.confirm}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
 
-      <input
-        type="submit"
-        value="Registrarse"
-        className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-      />
-    </form>
+        <input
+          type="submit"
+          value="Registrarse"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
 
-    <nav className="lg:flex lg:justify-between">
-      <Link
-        className="block text-center my-5 text-slate-500 text-sm"
-        to="/"
-      >
-        ¿Ya tienes una cuenta?, Inicia sesión
-      </Link>
+      <nav className="lg:flex lg:justify-between">
+        <Link className="block text-center my-5 text-slate-500 text-sm" to="/">
+          ¿Ya tienes una cuenta?, Inicia sesión
+        </Link>
+      </nav>
+    </>
+  );
+};
 
-    </nav>
-  </>
-  )
-}
-
-export default Register
+export default Register;
