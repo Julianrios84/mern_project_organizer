51c53fb8111c9b5a73b20fd791c8b4ab1a40ef0f
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-28 20:36:40 -0500

    Creando pagina de nueva contraseña y confirmar la cuenta

--- frontend/src/pages/auth/confirm.page.jsx
+++ frontend/src/pages/auth/confirm.page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Confirm = () => {
   return (
-    <div>Confirm</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl font-sansita">
+        Confirma tu cuenta y comienza a crear tus {''}
+        <span className="text-slate-700 font-sansita">projectos</span>
+      </h1>
+    </>
+  );
+};
 
-export default Confirm
+export default Confirm;
--- frontend/src/pages/auth/password/reset.page.jsx
+++ frontend/src/pages/auth/password/reset.page.jsx
@@ -4,41 +4,31 @@ const ResetPassword = () => {
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl font-sansita">
-        Recupera tu acceso y no pierdas tus {''}
+        Restablece tu password y no pierdas tus {''}
         <span className="text-slate-700 font-sansita">projectos</span>
       </h1>
       <form className="my-10 bg-white shadow rounded-lg p-10">
-        
         <div className="my-5">
           <label
             className="capitalize text-gray-600 block text-xl font-bold"
-            htmlFor="email"
+            htmlFor="password"
           >
-            Email
+            Nueva contraseña
           </label>
           <input
-            id="email"
-            type="email"
-            placeholder="Email de registro"
+            id="password"
+            type="password"
+            placeholder="Contaseña"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
           />
         </div>
-       
+
         <input
           type="submit"
-          value="Enviar Instrucciones"
+          value="Guardar cambios"
           className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
         />
       </form>
-
-      <nav className="lg:flex lg:justify-between">
-        <Link
-          className="block text-center my-5 text-slate-500 text-sm"
-          to="/register"
-        >
-          ¿Ya tienes una cuenta?, Inicia sesión
-        </Link>
-      </nav>
     </>
   );
 };
