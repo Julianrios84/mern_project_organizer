8db354f0500def709fb3f871c0ca20d08f4cdf20
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-30 23:10:26 -0500

    Solucionar el Detalle con la Fecha

--- frontend/src/helpers/formatterdate.helper.jsx
+++ frontend/src/helpers/formatterdate.helper.jsx
@@ -1,5 +1,5 @@
 export const formatterDate = (date) => {
-  const d = new Date(date);
+  const d = new Date(date.split('T')[0].split('-'));
   return d.toLocaleDateString('es-ES', {
     weekday: 'long',
     year: 'numeric',
