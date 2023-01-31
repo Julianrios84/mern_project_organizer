266f2e7246c5bcdf5d238eac9c09c32c1176b07d
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-30 19:21:46 -0500

    Añadiendo Headless UI

--- frontend/src/pages/project/project.page.jsx
+++ frontend/src/pages/project/project.page.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import ModalTask from '../../components/task/modal.component';
 import useProject from '../../hooks/project.hook';
 
 const Project = () => {
@@ -48,18 +49,22 @@ const Project = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          fill="none"
           viewBox="0 0 24 24"
-          fill="currentColor"
+          strokeWidth={1.5}
+          stroke="currentColor"
           className="w-5 h-5"
         >
           <path
-            fillRule="evenodd"
-            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-            clipRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
         Nueva tarea
       </button>
+
+      <ModalTask />
     </>
   );
 };
