32b1d31b0af1a9cbe6f71ae9a5e97c6c88861fec
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-02-04 17:34:57 -0500

    Conectarnos a Socket.io

--- frontend/src/pages/project/projects.page.jsx
+++ frontend/src/pages/project/projects.page.jsx
@@ -1,16 +1,22 @@
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 import useProject from '../../hooks/project.hook';
 import PreviewProject from '../../components/project/preview.component';
 import Alert from '../../components/alert.component';
 
+let socket;
+
 const Projects = () => {
   const { projects, alert } = useProject();
 
-
+  useEffect(() => {
+    socket = io(import.meta.env.VITE_BACKEND_URL);
+  }, []);
 
   return (
     <>
       <h1 className="text-4xl font-black font-sansita">Proyectos</h1>
-      { alert.message && <Alert alert={alert} />}
+      {alert.message && <Alert alert={alert} />}
       <div className="bg-white shadow mt-10 rounded-lg p-5">
         {projects.length ? (
           projects.map((project) => (
@@ -20,7 +26,6 @@ const Projects = () => {
           <p className="text-center text-gray-600">No hay proyectos aún</p>
         )}
       </div>
-
     </>
   );
 };
